2151dca334104befd9c9de4f2b5b3ec3e37e9385
Sergey <segarik93@gmail.com>
2022-12-13 19:13:27 +0300

    DZ.8.End

---
+++ Task58/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+Console.Clear();
+
+void PrintArrya(int[,] arg)  
+{
+    for (int i = 0; i < arg.GetLength(0); i++) 
+    {
+        for (int j = 0; j < arg.GetLength(1); j++) 
+        {
+            Console.Write($"{arg[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArrya(int[,] arg)
+{
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        for (int j = 0; j < arg.GetLength(1); j++) 
+        {
+            arg[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void DerivativeArrya(int[,] arg, int[,] arg1)
+{
+    int[,] result = new int[arg.GetLength(0), arg1.GetLength(0)]; 
+
+    if (arg.GetLength(1) == arg1.GetLength(0)) 
+    {
+        for (int i = 0; i < arg.GetLength(0); i++) 
+        {
+            for (int j = 0; j < arg1.GetLength(1); j++)  
+            {
+                for (int l = 0; l < result.GetLength(0); l++)
+                {
+                    result[i, j] += arg[i, l] * arg1[l, j];
+                }
+                Console.Write($"{result[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+    else Console.WriteLine("Столбцы  не равны строкам");
+
+}
+
+
+
+Console.Write("Введите длину строки ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите длину столбца ");
+int colums = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второй строки ");
+int row1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второго столбца ");
+int colums1 = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int[row, colums];
+int[,] arrya = new int[row1, colums1];
+
+FillArrya(arr);
+FillArrya(arrya);
+
+PrintArrya(arr);
+Console.WriteLine();
+PrintArrya(arrya);
+Console.WriteLine("Приозведение матриц ");
+DerivativeArrya(arr, arrya);
---
+++ Task60/Program.cs
@@ -0,0 +1,78 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+
+Console.Clear();
+
+void PrintArrya(int[,,] arg)  
+{
+    for (int i = 0; i < arg.GetLength(0); i++) 
+    {
+        for (int j = 0; j < arg.GetLength(1); j++) 
+        {
+            for (int k = 0; k < arg.GetLength(2); k++)
+            {
+                Console.Write($" {arg[i, j, k]} - {(i, j, k)} ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArrya(int[,,] arg)
+{
+    int[] repeat = new int[arg.GetLength(0) * arg.GetLength(1) * arg.GetLength(2)]; 
+    int cout = 0; // счетчик
+
+    for (int i = 0; i < arg.GetLength(0); i++) 
+    {
+        for (int j = 0; j < arg.GetLength(1); j++)  
+        {
+            for (int k = 0; k < arg.GetLength(2);)
+            {
+                repeat[cout] = new Random().Next(10, 100);  
+                bool checkRepeat = true;  
+
+                for (int l = 0; l < cout; l++) 
+                {
+                    if (repeat[cout] == repeat[l]) 
+                    {
+                        checkRepeat = false; 
+                        repeat[cout] = 0;  
+                        break;             
+                    }
+                }
+                if (checkRepeat)  
+                {
+                    arg[i, j, k] = repeat[cout]; 
+                    cout++;
+                    k++;
+                }
+
+            }
+        }
+    }
+
+
+}
+
+
+Console.Write("Введите длину строки ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите длину столбца ");
+int colums = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите длину глубины ");
+int depth = Convert.ToInt32(Console.ReadLine());
+
+int[,,] arr = new int[row, colums, depth];
+
+FillArrya(arr);
+PrintArrya(arr);
